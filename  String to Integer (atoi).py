3e2b57a4a65f86b433ee3f09d11990b35b90dd46
swadhinbiswas <swadhinbiswas.cse@gmail.com>
2024-04-19 15:00:15 +0600

    update

---  String to Integer (atoi).py
+++  String to Integer (atoi).py
@@ -1,29 +1,3 @@
 class Solution:
     def myAtoi(self, s: str) -> int:
-       num:int = 0
-       
-       for i in range(len(s)):
-          sign:int = 1
-          if s[i]=='-':
-            sign = -1
-          elif s[i]==' ':
-            continue
-          elif s[i].isdigit():
-            num = num * 10 + int(s[i])
-            if num > 2**31-1:
-              return 2**31-1
-            if num < -2**31:
-              return -2**31
-          else:
-            break
-          
-          return num * sign
-        
-        
-        
-
-# Time Complexity: O(n)
-
-
-c=Solution()
-print(c.myAtoi("4245 fuck you")) #42
+      
--- Zigzag Conversion.py
+++ Zigzag Conversion.py
@@ -1,21 +1,16 @@
 class Solution:
     def convert(self, s: str, numRows: int) -> str:
-      if numRows == 1:
+      if numRows==1:
         return s
-      res=""
-      for r in range(numRows):
-        increment = 2*(numRows-1)
-        for i in range(r,len(s),increment):
-          res+=s[i]
-          if r!=0 and r!=numRows-1 and i+increment-2*r<len(s):
-            res+=s[i+increment-2*r]
-            if (r>0 and r<numRows-1 and i+increment-2*r<len(s)):
-              res+=s[i+increment-2*r]
-              
-      return res 
-    
+      listy=[""]*numRows
+      index=0
+      step=1
+      for i in s:
+        listy[index]+=i
+        if index==0:
+          step=1
+        elif index==numRows-1:
+          step=-1
+        index+=step
+      return "".join(listy)
 
-
-
-c=Solution()
-print(c.convert("PAYPALISHIRING",3)) #PAHNAPLSIIGYIR
