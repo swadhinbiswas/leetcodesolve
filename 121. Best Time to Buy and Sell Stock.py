6cdab409e7aa5592e70e5887f5846ce3fc1642c1
swadhinbiswas <swadhinbiswas.cse@gmail.com>
2025-01-30 16:56:57 +0600

    update leetcode

--- 121. Best Time to Buy and Sell Stock.py
+++ 121. Best Time to Buy and Sell Stock.py
@@ -1,2 +1,17 @@
+from typing import List
 class Solve:
+    def maxProfit(self,prices:List[int])->int:
+        maxProfit=0
+        bestbuy=prices[0]
+        for i in range(1,len(prices)):
+            if prices[i]>bestbuy:
+                maxProfit=max(maxProfit,prices[i]-bestbuy)
 
+            bestbuy=min(maxProfit,prices[i])
+            
+        return maxProfit
+
+
+
+
+    
